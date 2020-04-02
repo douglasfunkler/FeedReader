faa4c1549fd19d49a9248fd81fd83764cab57f22
douglasfunkler <douglasfunkler@gmail.com>
2020-04-02 23:03:06 +0100

    Structured established
    All set up. Structure ready to implement.

--- src/algorithmsandconstructs/s2019325/Feed.java
+++ src/algorithmsandconstructs/s2019325/Feed.java
@@ -3,10 +3,37 @@ package algorithmsandconstructs.s2019325;
 import algorithmsandconstructs.FeedInterface;
 import algorithmsandconstructs.FeedItem;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public abstract class Feed implements FeedInterface {
 
-    ArrayList<FeedItem> feedItems = new ArrayList();
+    @Override
+    public void addItem(FeedItem item) {
+        ArrayList<FeedItem> arrayList = new ArrayList<>();
+        //arrayList.
+    }
 
+    @Override
+    public Collection<String> listTitles() {
+
+        return null;
+    }
+
+    @Override
+    public FeedItem getItem(String title) {
+        return null;
+    }
+
+    @Override
+    public int numItems() {
+        return 0;
+    }
+
+    @Override
+    public Collection<FeedItem> findItems(String keyword) {
+        return null;
+    }
 }
--- src/algorithmsandconstructs/s2019325/FeedFactory.java
+++ src/algorithmsandconstructs/s2019325/FeedFactory.java
@@ -1,6 +1,18 @@
 package algorithmsandconstructs.s2019325;
 
 import algorithmsandconstructs.FeedFactoryInterface;
+import algorithmsandconstructs.FeedInterface;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public abstract class FeedFactory implements FeedFactoryInterface {
+
+    @Override
+    public FeedInterface createFeed(BufferedReader in) throws IOException {
+
+        
+        return null;
+    }
 }
